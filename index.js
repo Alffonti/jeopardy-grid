2f6090617e8f929ae09d930c75fad13486a6f8ce
Alffonti <alffonti@gmail.com>
2022-07-12 19:58:43 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// https://jservice.io/
+
+let categoryCount = 5
+let categoryOffset = Math.floor(Math.random() * 10000) + 1
+
+async function getCategories(count, offset) {
+    let response = await fetch(`https://jservice.io/api/categories?count=${count}&offset=${offset}`)
+    let data = await response.json()
+    return data
+}
+
+function getClueHtml(clueValue) {
+    return `<div class="my-category-clue" style="grid-row-start: ${clueValue / 100 + 1}">$${clueValue}</div>`
+}
+
+function getCategoryHtml(category) {
+    return `
+        <div class="my-category-title">${category.title}</div>
+        ${getClueHtml(100)}
+        ${getClueHtml(200)}
+        ${getClueHtml(300)}
+        ${getClueHtml(400)}
+    `
+}
+
+getCategories(categoryCount, categoryOffset).then(categories => {
+    console.log(categories)
+    document.body.innerHTML = `<div class="board">
+        ${categories.map(getCategoryHtml).join('')}
+    </div>`
+})
